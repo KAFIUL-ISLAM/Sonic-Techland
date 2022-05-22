b34cd519a00fed9df936e4ccca8bbfa709807077
Al Kafi <alkafi28@gmail.com>
2022-05-23 01:20:35 +0600

    purchase functionality added

--- src/Components/PurchasePage/Purchase.js
+++ src/Components/PurchasePage/Purchase.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import Footer from '../CommonComp/Footer';
 import Header from '../CommonComp/Header';
 
 const Purchase = () => {
 
-    const [quantity, setQuantity] = useState(0); //minimum default value
+    const [user] = useAuthState(auth);
+    const [quantity, setQuantity] = useState(2); //minimum default value
     const [price, setPrice] = useState(0); //minimum default value
     useEffect(() => {
         setPrice(quantity * 2);
-    },[quantity])
+    }, [quantity]);
 
+    const handleSubmit = () =>{
+        console.log();
+    }
     const increaseQuantity = () => {
         setQuantity(quantity + 1);
     }
@@ -23,11 +29,19 @@ const Purchase = () => {
     return (
         <div>
             <Header></Header>
-            <div className='min-h-screen p-2 md:p-8 flex'>
-                <div className='flex-1'>
-
+            <div className='min-h-screen p-2 md:p-8 grid md:grid-cols-5 grid-cols-1 gap-8'>
+                <div className='col-span-3 grid grid-cols-2'>
+                    <div className='border'>
+                        <h1>first one</h1>
+                    </div>
+                    <div className='border row-span-2'>
+                        <h1>second one</h1>
+                    </div>
+                    <div className='border'>
+                        <h1>thrid one</h1>
+                    </div>
                 </div>
-                <form className='flex-1'>
+                <form onSubmit={handleSubmit} className='col-span-2'>
                     <div className='mb-8 space-y-4'>
                         <p>Selected product: Demo product</p>
                         <p>Price per unit: $979</p>
@@ -42,16 +56,16 @@ const Purchase = () => {
                         </div>
                     </div>
                     <div class="relative z-0 w-full mb-6 group">
-                        <input type="email" name="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                        <input type="email" name="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" value={user.email} disabled />
                         <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                     </div>
                     <div class="relative z-0 w-full mb-6 group">
-                        <input type="text" name="floating_name" id="floating_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input type="text" name="floating_name" id="floating_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" defaultValue={user.displayName} required />
                         <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
                     </div>
                     <div class="grid xl:grid-cols-2 xl:gap-6">
                         <div class="relative z-0 w-full mb-6 group">
-                            <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <input type="number" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                             <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number</label>
                         </div>
                         <div class="relative z-0 w-full mb-6 group">
