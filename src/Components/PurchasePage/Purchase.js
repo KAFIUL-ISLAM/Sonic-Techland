f022d9c529d50d2ee73523f9a7dd6c3d0003cc28
Al Kafi <alkafi28@gmail.com>
2022-05-23 00:37:46 +0600

    purchase page added

--- src/Components/PurchasePage/Purchase.js
+++ src/Components/PurchasePage/Purchase.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Footer from '../CommonComp/Footer';
 import Header from '../CommonComp/Header';
 
 const Purchase = () => {
+
+    const [quantity, setQuantity] = useState(0); //minimum default value
+    const [price, setPrice] = useState(0); //minimum default value
+    useEffect(() => {
+        setPrice(quantity * 2);
+    },[quantity])
+
+    const increaseQuantity = () => {
+        setQuantity(quantity + 1);
+    }
+    const decreaseQuantity = () => {
+        setQuantity(quantity - 1);
+    }
+    const handleChange = e => {
+        setQuantity(e.target.value);
+    }
+
     return (
         <div>
             <Header></Header>
-            <h1>this is purchase page</h1>
+            <div className='min-h-screen p-2 md:p-8 flex'>
+                <div className='flex-1'>
+
+                </div>
+                <form className='flex-1'>
+                    <div className='mb-8 space-y-4'>
+                        <p>Selected product: Demo product</p>
+                        <p>Price per unit: $979</p>
+                        <p>Total price: ${price}</p>
+                        <div className='flex gap-2 items-center'>
+                            <label htmlFor="quantity">Quantity: </label>
+                            <div className='border-2 border-gray-300'>
+                                <i onClick={decreaseQuantity} class="fa-solid fa-minus px-1 cursor-pointer"></i>
+                                <input type="number" name='quantity' className='w-20 h-8 border-gray-300 border-l-2 border-r-2 border-y-0 text-center' value={quantity} onChange={handleChange} />
+                                <i onClick={increaseQuantity} class="fa-solid fa-plus px-1 cursor-pointer"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="email" name="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="floating_name" id="floating_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
+                    </div>
+                    <div class="grid xl:grid-cols-2 xl:gap-6">
+                        <div class="relative z-0 w-full mb-6 group">
+                            <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number</label>
+                        </div>
+                        <div class="relative z-0 w-full mb-6 group">
+                            <input type="text" name="floating_company" id="floating_company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                            <label for="floating_company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company </label>
+                        </div>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <textarea type="text" name="floating_address" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_address" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
+                    </div>
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit a booking</button>
+                </form>
+            </div>
             <Footer></Footer>
         </div>
     );
