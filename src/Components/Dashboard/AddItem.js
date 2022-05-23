fe3d79b3e1553625c178506741b178550cf3700e
Al Kafi <alkafi28@gmail.com>
2022-05-24 01:24:07 +0600

    item added functionality done

--- src/Components/Dashboard/AddItem.js
+++ src/Components/Dashboard/AddItem.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 const AddItem = () => {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const imageStorageKey = 'bcf540dfd62d4ac1eef317a2f57047ed';
     const onSubmit = data => {
-        console.log(data);
+        const formData = new FormData();
+        formData.append('image', data.image[0]);
+        const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(result => {
+                const img = result.data.url;
+                const item = {
+                    name: data.name,
+                    price: data.price,
+                    minOrderQuantity: data.minOrderQuantity,
+                    quantity: data.quantity,
+                    description: data.description,
+                    image: img
+                }
+                fetch('http://localhost:5000/parts', {
+                    method: 'POST',
+                    headers: {
+                        "content-type": "application/json"
+                        //admin verify
+                    },
+                    body: JSON.stringify(item)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.insertedId) {
+                            toast.success('New parts added successfully');
+                            reset();
+                        };
+                    })
+            });
     };
 
     return (
@@ -17,11 +53,11 @@ const AddItem = () => {
                 {errors.price && <span className="text-red-600">*This field is required</span>}
                 <div className="flex gap-2">
                     <div className="flex-1">
-                        <input type="number"  className="input input-bordered w-full" {...register("minOrderQuantity", { required: true })} placeholder="Minimum order quantity" />
+                        <input type="number" className="input input-bordered w-full" {...register("minOrderQuantity", { required: true })} placeholder="Minimum order quantity" />
                         {errors.price && <span className="text-red-600">*This field is required</span>}
                     </div>
                     <div className="flex-1">
-                        <input type="number"  className="input input-bordered w-full" {...register("quantity", { required: true })} placeholder="Stock quantity" />
+                        <input type="number" className="input input-bordered w-full" {...register("quantity", { required: true })} placeholder="Stock quantity" />
                         {errors.price && <span className="text-red-600">*This field is required</span>}
                     </div>
                 </div>
